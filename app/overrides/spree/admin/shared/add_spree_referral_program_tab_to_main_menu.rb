7507773f1c0513a061dde3dbd308c8fb9e2800cd
tniezg <tomek.niezgoda@mail.com>
2021-06-06 15:10:44 +0200

    Rewrite Deface menu overrides for Spree 4

---
+++ app/overrides/spree/admin/shared/add_spree_referral_program_tab_to_main_menu.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(
+  virtual_path: 'spree/admin/shared/_main_menu',
+  name: 'spree_referral_program_add_home_page_tab_to_main_menu',
+  insert_bottom: 'nav',
+  partial: 'spree_referral_program/spree/admin/homepage_tabs'
+)
