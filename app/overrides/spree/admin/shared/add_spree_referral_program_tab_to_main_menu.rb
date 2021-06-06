6c9b34f52a1198cc169fdea9e44e175bdb703e82
tniezg <tomek.niezgoda@mail.com>
2021-06-06 15:38:23 +0200

    Adjust Spree admin menu link to rewards

--- app/overrides/spree/admin/shared/add_spree_referral_program_tab_to_main_menu.rb
+++ app/overrides/spree/admin/shared/add_spree_referral_program_tab_to_main_menu.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/admin/shared/_main_menu',
   name: 'spree_referral_program_add_home_page_tab_to_main_menu',
   insert_bottom: 'nav',
-  partial: 'spree_referral_program/spree/admin/homepage_tabs'
+  partial: 'spree/admin/shared/spree_referral_program_homepage_tabs'
 )
